cf0d32730ab2b452c19ad225183ba3dabe8595ed
zhangyinzhou <zhangyinzhou@zonghenggroup.com.cn>
2024-03-12 10:43:25 +0800

    feat: init project.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+const App = () => {
+  return (
+    <div className="app">
+      this is app
+      <Button type="primary">测试按键</Button>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 引入 React 核心库
+import React from 'react'
+// 引入 React 扩展库
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 引入 App 组件
+import App from './App'
+
+const root = ReactDOM.createRoot(document.querySelector(".root"))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
