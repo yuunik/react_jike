2915f9a21beebc7299aa99269bc131fbea7f088d
zhangyinzhou <zhangyinzhou@zonghenggroup.com.cn>
2024-03-14 10:01:04 +0800

    feat: Publish - 筛选功能实现

--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -10,11 +10,14 @@ import {
 } from "antd";
 // 时间选择器汉化包
 import locale from "antd/es/date-picker/locale/zh_CN";
+import "dayjs/locale/zh-cn";
 // 导入资源
 import { Table, Tag, Space } from "antd";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import img404 from "@/assets/error.png";
 import { nanoid } from "nanoid";
+import dayjs from "dayjs";
+
 import { useEffect, useState } from "react";
 import { getArticleListAPI } from "@/apis/article";
 
@@ -44,7 +47,7 @@ const Article = () => {
     {
       title: "状态",
       dataIndex: "status",
-      render: status => articleStatusMap[status],
+      render: (status) => articleStatusMap[status],
     },
     {
       title: "发布时间",
@@ -89,24 +92,54 @@ const Article = () => {
   // 文章总数
   const [articleCount, setArticleCount] = useState(0);
 
+  // 请求数据
+  const [reqData, setReqData] = useState({
+    // 文章状态
+    status: '',
+    // 频道分类
+    channel_id: '',
+    // 起始时间
+    begin_pubdate: '',
+    // 结束时间
+    end_pubdate: '',
+    // 当前页码
+    page: 1,
+    // 当前页条数
+    per_page: 4
+  })
+
   // 组件挂载时调用
   useEffect(() => {
+    // 获取文章列表
     const getArticleList = async () => {
       // 调用接口, 获取文章列表
-      const result = await getArticleListAPI();
+      const result = await getArticleListAPI(reqData);
       // 保存文章列表
       setArticleList(result.data.results);
       // 保存文章总数
       setArticleCount(result.data.total_count);
     };
+    // 调用接口, 获取文章列表
     getArticleList();
-  }, []);
+  }, [reqData]);
 
   // 文章状态映射对象
   const articleStatusMap = {
     1: <Tag color="warning">待审核</Tag>,
-    2: <Tag color="success">审核通过</Tag>
-  }
+    2: <Tag color="success">审核通过</Tag>,
+  };
+
+  // 筛选的回调函数
+  const onSubmit = ({ status, date, channel_id }) => {
+    // 修改请求参数
+    setReqData({
+      ...reqData,
+      status,
+      channel_id,
+      begin_pubdate: dayjs(date[0]).format("YYYY-MM-DD"),
+      end_pubdate: dayjs(date[1]).format("YYYY-MM-DD"),
+    });
+  };
 
   return (
     <div>
@@ -121,7 +154,7 @@ const Article = () => {
         }
         style={{ marginBottom: 20 }}
       >
-        <Form initialValues={{ status: "" }}>
+        <Form initialValues={{ status: "" }} onFinish={onSubmit}>
           <Form.Item label="状态" name="status">
             <Radio.Group>
               <Radio value={""}>全部</Radio>
